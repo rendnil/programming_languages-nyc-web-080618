507cb806c9818015375798f73a6c7201edead02f
Andrew Lindner <andrew.lindner.15@gmail.com>
2018-07-20 05:21:42 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,33 @@
+require "pry"
+
 def reformat_languages(languages)
-  # your code here
+  new_hash = { }
+  
+  languages.each do |style, data|
+    data.each do |language, attribute|
+      new_hash[language] = attribute
+    end
+  end
+  
+  ###adding the empty style key/value
+  new_hash.each do |language, attributes|
+      attributes[:style] = [ ]
+      
+  end
+  
+  #filling out style by reiterating through
+  languages.each do |style, data|
+    data.each do |original_language, attribute|
+      new_hash.each do |new_language, attributes|
+        if original_language == new_language
+         attributes[:style].push(style)
+      end
+    end    
+  end
 end
+  
+  
+  
+  new_hash
+end
+
