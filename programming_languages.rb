79cb0940db7b7c68bfd097d11ef308c49862b2d2
Andrew Lindner <andrew.lindner.15@gmail.com>
2018-07-20 05:32:26 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -2,8 +2,8 @@ def reformat_languages(languages)
    new_languages = {}
    languages.each do |type, hash|
      hash.each do |name, attributes|
-       new_languages[name] ||= attributes
-       new_languages[name][:style] ||= []
+       new_languages[name] = attributes
+       new_languages[name][:style] = []
        new_languages[name][:style] << type
      end
    end
